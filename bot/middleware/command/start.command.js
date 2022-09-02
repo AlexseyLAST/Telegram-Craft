014b74c99694b2960862f0e18ba90081b3b02fac
ALEXANDR PETRIN <dataarrayvlog@gmail.com>
2022-09-02 22:36:45 +0300

    added telegraf-i18n

--- bot/middleware/command/start.command.js
+++ bot/middleware/command/start.command.js
@@ -1,5 +1,17 @@
 const { bot } = require("../../connections/token.connection");
 const { saveUser } = require("../../common/sequelize/user-model.sequelize");
+const TelegrafI18n = require("telegraf-i18n");
+const path = require("path");
+
+const i18n = new TelegrafI18n({
+  directory: path.resolve("./bot/locales"),
+  useSession: true,
+  sessionName: "sessionLanguage",
+  allowMissing: false,
+  defaultLanguage: "ru",
+});
+
+bot.use(i18n);
 
 module.exports = bot.start(async (ctx) => {
   try {
@@ -8,6 +20,17 @@ module.exports = bot.start(async (ctx) => {
     const result = await saveUser(String(id), username);
     console.log(result);
 
+    // console.log(ctx.i18n)
+    // console.log(ctx.i18n.locale())
+
+    const a = ctx.i18n.locale();
+    ctx.i18n.locale(a);
+
+    // "ru"
+
+    const myMessage = ctx.i18n.t("start.text");
+    await ctx.reply(myMessage);
+    
     return;
   } catch (err) {
     console.log(err);
